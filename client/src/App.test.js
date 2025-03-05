1b5a827d18a29ffc542db82ba54cc9b3818b1a3d
hector360 <hector360_m@hotmail.com>
2025-03-05 01:29:40 -0500

    saving data2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+
+// Mock de axios
+jest.mock('axios');
 
 test('renders learn react link', () => {
+  // Mock de la respuesta de la API
+  axios.get.mockResolvedValue({ data: { /* tus datos mockeados */ } });
+
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
